f191f9456d2016ee691ecd5d6fc021be65ffcc9d
Debangshu <debangshu182002@gmail.com>
2022-07-02 22:57:49 +0530

    css and header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Header from "./components/Header"
+
 function App() {
     return (
+        <>
+        <Header/>
         <div className="container">
             <h1>My app</h1>
         </div>
+        </>
         )
 }
 
