d3720aefd3e03d8ab8c857a13caa9e85bd1008fc
Debangshu <debangshu182002@gmail.com>
2022-07-03 23:30:49 +0530

    feedback cards , rating and no of reviews

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import {useState} from 'react'
 import Header from "./components/Header"
+import FeedbackData from "./data/FeedBack"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackStats from './components/FeedbackStats'
 
 function App() {
+
+    const [feedback,setFeedback] = useState(FeedbackData)
+
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are You sure you want to delete')){
+
+            setFeedback(feedback.filter((item) => item.id !==id))
+        }
+        
+    }
+
+
     return (
         <>
         <Header/>
         <div className="container">
-            <h1>My app</h1>
+            <FeedbackStats feedback = {feedback}/>
+            <FeedbackList feedback={feedback}
+            handleDelete = {deleteFeedback} />
         </div>
         </>
         )
---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import FeedbackItem from './FeedbackItem'
+
+function feedbackList({feedback, handleDelete}) {
+
+    if(!feedback || feedback.length === 0)
+    {
+        return(
+            <p>No Feedbacks</p>
+        )
+    }
+
+  return (
+    <div className='feedback-list'>
+        {feedback.map((item) =>(
+            <FeedbackItem key = {item.id} 
+            item = {item}
+            handleDelete = {handleDelete}
+            />
+
+        ))}
+    </div>
+  )
+}
+feedbackList.propTypes = {
+    feedback: PropTypes.array.isRequired
+}
+
+
+
+export default feedbackList
